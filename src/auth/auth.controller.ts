0e3a7a8c538fa1398719aacc2aeb0b59078b28e4
Dip Vachhani <dv.dipvachhani@gmail.com>
2022-06-26 09:55:46 +0530

    | Add | Auth dto |

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log('🚀 ~ AuthController ~ dto', dto);
     return this.authService.signup();
   }
 
